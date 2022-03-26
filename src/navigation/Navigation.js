4fa2adac9075d3de8d860dfbb1c36eaf953e31b8
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2022-03-25 22:11:47 -0500

    Agregamos la visualización con iconos en el tab menú

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,9 +1,11 @@
-import { Text, View } from "react-native";
 import React, { Component } from "react";
+import { Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import FavoriteScreen from "../screens/FavoriteScreen";
 import PokedexScreen from "../screens/PokedexScreen";
 import AccountScreen from "../screens/AccountScreen";
+import pokeball from "../assets/pokeball.png";
 
 const Tab = createBottomTabNavigator();
 
@@ -11,10 +13,41 @@ export default class Navigation extends Component {
   render() {
     return (
       <Tab.Navigator>
-        <Tab.Screen name="Favorite" component={FavoriteScreen} />
-        <Tab.Screen name="Pokedex" component={PokedexScreen} />
-        <Tab.Screen name="Account" component={AccountScreen} />
+        <Tab.Screen
+          name="Favorite"
+          component={FavoriteScreen}
+          options={{
+            tabBarLabel: "Favoritos",
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="heart" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Pokedex"
+          component={PokedexScreen}
+          options={{
+            tabBarLabel: "",
+            tabBarIcon: () => renderPokeball(),
+          }}
+        />
+        <Tab.Screen
+          name="Account"
+          component={AccountScreen}
+          options={{
+            tabBarLabel: "Mi Cuenta",
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="user" color={color} size={size} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     );
   }
+}
+
+function renderPokeball() {
+  return (
+    <Image source={pokeball} style={{ width: 75, height: 75, top: -15 }} />
+  );
 }
