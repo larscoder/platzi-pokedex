ef53a5cd465d07a6dbf856ada73b5d2d83d9e88d
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2022-03-26 12:22:28 -0500

    Agregamos la funcionalidad para traer el listado de Pokemons

--- src/screens/PokedexScreen.js
+++ src/screens/PokedexScreen.js
@@ -1,7 +1,23 @@
+import React, { useState, useEffect } from "react";
 import { Button, Text, View } from "react-native";
-import React from "react";
+import { getPokemonsApi } from "../api/pokemon";
 
 export default function PokedexScreen(props) {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View>
       <Text>PokedexScreen</Text>
