7df84afa49933dc10e1324504f5fc19029f03fe4
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2022-03-29 19:56:32 -0500

    Maquetación del Pokemon: barra de navegación

--- src/screens/PokemonScreen.js
+++ src/screens/PokemonScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Text, ScrollView } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import { getPokemonDetailIdApi } from "../api/pokemon";
 import Header from "../components/Pokemon/Header";
 import Stats from "../components/Pokemon/Stats";
@@ -8,16 +9,29 @@ import Type from "../components/Pokemon/Type";
 export default function PokemonScreen(props) {
   const {
     navigation,
-    route: {
-      params: { id },
-    },
+    route: { params },
   } = props;
   const [pokemon, setPokemon] = useState(null);
 
   useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          color="#ffffff"
+          size={20}
+          style={{ marginLeft: 5 }}
+          onPress={navigation.goBack}
+        />
+      ),
+    });
+  }, [navigation, params]);
+
+  useEffect(() => {
     (async () => {
       try {
-        const response = await getPokemonDetailIdApi(id);
+        const response = await getPokemonDetailIdApi(params.id);
         setPokemon(response);
       } catch (error) {
         console.error(error);
