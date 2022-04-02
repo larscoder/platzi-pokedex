cd9c9cbd8d5b1af5475a42f3af737d1464c9ab0e
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2022-04-02 18:32:27 -0500

    useAuth: manejo de sesiones con Custom Hooks y React Context

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -10,9 +10,11 @@ import {
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { user, userDetail } from "../../utils/constants";
+import useAuth from "../../hooks/useAuth";
 
 export default function LoginForm() {
   const [error, setError] = useState("");
+  const { login } = useAuth();
 
   const formik = useFormik({
     initialValues: initialValues(),
@@ -24,7 +26,7 @@ export default function LoginForm() {
       if (username !== user.username || password !== user.password) {
         setError("El usuario o contraseña no son correctos");
       } else {
-        console.log("Iniciamos sesión, ", userDetail);
+        login(userDetail);
       }
     },
   });
--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Text, View } from "react-native";
 import LoginForm from "../components/Auth/LoginForm";
 import UserData from "../components/Auth/UserData";
+import useAuth from "../hooks/useAuth";
 
 export default function AccountScreen(props) {
-  const auth = null;
+  const { auth } = useAuth();
 
   return <View>{auth ? <UserData /> : <LoginForm />}</View>;
 }
